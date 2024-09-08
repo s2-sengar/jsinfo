e1bad9312292d730c19a20d5493565615fb95b06
s2-sengar <sudhanshu.iem2k18@gmail.com>
2024-09-08 18:13:29 +0530

    implement level order traversal generic tree

---
+++ DSA/NTree/7. LevelOrderTraversalGenericTree.js
@@ -0,0 +1,60 @@
+class TreeNode {
+  constructor(data) {
+    this.data = data;
+    this.child = [];
+  }
+}
+class GenericTee {
+  constructor() {
+    this.root = null;
+  }
+  buildGenericTree(eulerPath) {
+    let st = [];
+    for (let i = 0; i < eulerPath.length; i++) {
+      if (eulerPath[i] == -1) {
+        st.pop();
+        continue;
+      }
+      let node = new TreeNode(eulerPath[i]);
+      if (st.length == 0) {
+        this.root = node;
+      } else {
+        st[st.length - 1].child.push(node);
+      }
+      st.push(node);
+    }
+  }
+  displayGenericTree() {
+    this.displayGenericTreeHelper(this.root);
+  }
+  displayGenericTreeHelper(node) {
+    let nodeRes = `${node.data} -> `;
+    for (let i = 0; i < node.child.length; i++) {
+      nodeRes += `${node.child[i].data}, `;
+    }
+    nodeRes += '.';
+    console.log(nodeRes);
+    for (let i = 0; i < node.child.length; i++) {
+      this.displayGenericTreeHelper(node.child[i]);
+    }
+  }
+  levelOrderTraversalGenericTree(){
+    let qu=[];
+    qu.push(this.root);
+    let ans=''
+    while(qu.length>0){
+        let node=qu.shift();
+        for(let child of node.child){
+            qu.push(child);
+        }
+        ans+=node.data+', '
+    }
+    console.log(ans);
+  }
+}
+
+let genericTee = new GenericTee();
+let eulerPath = [10, 1, -1, 2, 6, -1, 8, -1, -1, 3, -1, 4, -1, -1];
+genericTee.buildGenericTree(eulerPath);
+genericTee.displayGenericTree();
+genericTee.levelOrderTraversalGenericTree();
